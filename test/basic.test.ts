fba794b8fe221c965b9ade778f9a986de5876f7f
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-29 07:17:55 -0500

    test folder, eslint changes only

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -4,13 +4,13 @@ import { $fetch, setup } from "@nuxt/test-utils/e2e"
 import { describe, expect, it } from "vitest"
 
 describe("ssr", async () => {
-	await setup({
-		rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
-	})
+  await setup({
+    rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
+  })
 
-	it("renders the index page", async () => {
-		// Get response to a server-rendered page with `$fetch`.
-		const html = await $fetch("/")
-		expect(html).toContain("<div>basic</div>")
-	})
+  it("renders the index page", async () => {
+    // Get response to a server-rendered page with `$fetch`.
+    const html = await $fetch("/")
+    expect(html).toContain("<div>basic</div>")
+  })
 })

825af14f02457a138bc8436ed2aa0f6e7cc29428
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-28 22:07:10 -0500

    #app module to build the library in a more typesafe way

---
+++ types/app.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @stylistic/semi */
+
+import type { Ref } from "vue";
+
+// types/app.d.ts
+declare module "#app" {
+
+	export function useState<T>(key: string, initialValue?: () => T): Ref<T>
+}
+
+export { };

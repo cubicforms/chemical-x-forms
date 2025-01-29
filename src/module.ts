d6cc889e515c8d1b1ec8595ab936be8817acc8f6
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-29 07:18:14 -0500

    restructure module.ts code, no implementation changes

--- src/module.ts
+++ src/module.ts
@@ -1,23 +1,27 @@
-import { addImportsDir, addPlugin, createResolver, defineNuxtModule } from "@nuxt/kit"
+import { addImportsDir, createResolver, defineNuxtModule } from "@nuxt/kit"
 
 // Module options TypeScript interface definition
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
-	meta: {
-		name: "chemical-x-forms",
-		configKey: "myModule",
-	},
-	// Default configuration options of the Nuxt module
-	defaults: {
-	},
-	setup(_options, _nuxt) {
-		const resolver = createResolver(import.meta.url)
+  meta: {
+    name: "chemical-x-forms",
+    configKey: "chemicalXForms",
+  },
+  defaults: {
+  },
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
 
-		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-		addPlugin(resolver.resolve("./runtime/plugin"))
+    // Adapters
+    addImportsDir(resolver.resolve("./runtime/adapters/zod"))
 
-		addImportsDir(resolver.resolve("./runtime/composables"))
-	},
+    // Core
+    const utilsDir = resolver.resolve("./runtime/lib/utils")
+    const helperComposablesDir = resolver.resolve("./runtime/lib/helper-composables")
+    const composablesDir = resolver.resolve("./runtime/lib/composables")
+
+    addImportsDir([utilsDir, composablesDir, helperComposablesDir])
+  },
 })

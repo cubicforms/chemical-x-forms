520726eca95a442f8d772962b30cc72cbbb183cb
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-29 07:17:32 -0500

    type/app.d.ts eslint change

--- types/app.d.ts
+++ types/app.d.ts
@@ -5,7 +5,7 @@ import type { Ref } from "vue";
 // types/app.d.ts
 declare module "#app" {
 
-	export function useState<T>(key: string, initialValue?: () => T): Ref<T>
+  export function useState<T>(key: string, initialValue?: () => T): Ref<T>
 }
 
 export { };

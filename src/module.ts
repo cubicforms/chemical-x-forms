4ceab22fb7968f15fd142f6ff31b885336d702a6
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-29 11:49:42 -0500

    update module.ts
    to test, do `pnpm prepack` and link the dist to another project
    (remember to add 'chemical-x-forms' to the modules array in your
    nuxt.config.ts file)

--- src/module.ts
+++ src/module.ts
@@ -15,13 +15,25 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url)
 
     // Adapters
-    addImportsDir(resolver.resolve("./runtime/adapters/zod"))
+    // addImportsDir(resolver.resolve("./runtime/adapters/zod"))
 
     // Core
-    const utilsDir = resolver.resolve("./runtime/lib/utils")
-    const helperComposablesDir = resolver.resolve("./runtime/lib/helper-composables")
-    const composablesDir = resolver.resolve("./runtime/lib/composables")
+    // const utilsDir = resolver.resolve("./runtime/lib/utils")
+    // const helperComposablesDir = resolver.resolve("./runtime/lib/helper-composables")
+    // const utilsDir = ""
+    // const helperComposablesDir = ""
+    // const composablesDir = resolver.resolve("./runtime/lib/composables")
 
-    addImportsDir([utilsDir, composablesDir, helperComposablesDir])
+    // addImportsDir([utilsDir, composablesDir, helperComposablesDir])
+    // addImportsDir(resolver.resolve("runtime"))
+    // addImportsDir(resolver.resolve("./runtime"))
+    addImportsDir(resolver.resolve("./runtime/composables"))
+    addImportsDir(resolver.resolve("./runtime/adapters/zod"))
+    // addImports({
+    //   name: "useForm", // name of the composable to be used
+    //   as: "useForm",
+    //   from: resolver.resolve("runtime/composables/use-form"), // path of composable
+    // })
+    // addImports(resolver.resolve("./runtime/lib/composables/use-form"))
   },
 })

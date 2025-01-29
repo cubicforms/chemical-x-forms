77912c913b47b2f2e4814d83e85db3a7f62f229a
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-29 07:16:31 -0500

    eslint changes to make editor experience a little better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,50 +3,51 @@ import { createConfigForNuxt } from "@nuxt/eslint-config/flat"
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
-	features: {
-		// Rules for module authors
-		tooling: true,
-		stylistic: {
-			arrowParens: false,
-			blockSpacing: true,
-			braceStyle: "stroustrup",
-			commaDangle: "always-multiline",
-			flat: true,
-			indent: "tab",
-			jsx: true,
-			pluginName: "@stylistic",
-			quoteProps: "consistent-as-needed",
-			quotes: "double",
-			semi: false,
-		},
-	},
-	dirs: {
-		src: [
-			"./playground",
-		],
-	},
+  features: {
+    // Rules for module authors
+    tooling: true,
+    stylistic: {
+      arrowParens: false,
+      blockSpacing: true,
+      braceStyle: "stroustrup",
+      commaDangle: "always-multiline",
+      flat: true,
+      indent: 2,
+      jsx: true,
+      pluginName: "@stylistic",
+      quoteProps: "consistent-as-needed",
+      quotes: "double",
+      semi: false,
+    },
+  },
+  dirs: {
+    src: [
+      "./playground",
+    ],
+  },
 })
-	.append(
-		{
-			rules: {
-				"import/order": [
-					"error",
-					{
-						"groups": [
-							"builtin",
-							"external",
-							"internal",
-							["sibling", "parent"],
-							"index",
-						],
-						"newlines-between": "always",
-						"alphabetize": {
-							order: "asc",
-							caseInsensitive: true,
-						},
-					},
-				],
-				"semi": "off",
-			},
-		},
-	)
+  .append(
+    {
+      rules: {
+        // "import/order": [
+        //   "error",
+        //   {
+        //     "groups": [
+        //       "builtin",
+        //       "external",
+        //       "internal",
+        //       ["sibling", "parent"],
+        //       "index",
+        //     ],
+        //     "newlines-between": "always",
+        //     "alphabetize": {
+        //       order: "asc",
+        //       caseInsensitive: true,
+        //     },
+        //   },
+        // ],
+        "semi": "off",
+        "import/order": "off",
+      },
+    },
+  )

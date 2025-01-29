3b6b128eb5938eb78ffda0682d0f8c22569c651f
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-28 22:09:59 -0500

    a basic preferred eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat"
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+	features: {
+		// Rules for module authors
+		tooling: true,
+		stylistic: {
+			arrowParens: false,
+			blockSpacing: true,
+			braceStyle: "stroustrup",
+			commaDangle: "always-multiline",
+			flat: true,
+			indent: "tab",
+			jsx: true,
+			pluginName: "@stylistic",
+			quoteProps: "consistent-as-needed",
+			quotes: "double",
+			semi: false,
+		},
+	},
+	dirs: {
+		src: [
+			"./playground",
+		],
+	},
+})
+	.append(
+		{
+			rules: {
+				"import/order": [
+					"error",
+					{
+						"groups": [
+							"builtin",
+							"external",
+							"internal",
+							["sibling", "parent"],
+							"index",
+						],
+						"newlines-between": "always",
+						"alphabetize": {
+							order: "asc",
+							caseInsensitive: true,
+						},
+					},
+				],
+				"semi": "off",
+			},
+		},
+	)

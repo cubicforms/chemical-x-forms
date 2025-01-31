693289ecaf3fe62ab2ec960e63455b10b1a24f14
Oswald Chisala <ochisala@canarytechnologies.com>
2025-01-31 10:42:19 -0500

    stop compiling lib folder and move types to collocated folder
    vite/rollup bundles the lib deps in the useForm entrypoint and moving
    types to collocated folder prevents us from accidentally attempting to
    bundle them, resulting in fewer files (generally good)

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ function collectTsFiles(
 }
 
 const runtimeDir = path.resolve(__dirname, "src/runtime")
-const libDir = path.resolve(__dirname, "src/lib")
+// const libDir = path.resolve(__dirname, "src/lib")
 const outputDir = path.resolve(__dirname, "dist/vite")
 
 function addBaseDirToEntrypoints(
@@ -50,7 +50,7 @@ function addBaseDirToEntrypoints(
 // Dynamically create entry points for both runtime and lib
 const entrypoints = {
   ...addBaseDirToEntrypoints(collectTsFiles(runtimeDir), "runtime"),
-  ...addBaseDirToEntrypoints(collectTsFiles(libDir), "lib"),
+  // ...addBaseDirToEntrypoints(collectTsFiles(libDir), "lib"),
 }
 
 export default defineConfig({
@@ -61,7 +61,8 @@ export default defineConfig({
     dts({
       include: [
         "src/runtime/**/*.ts",
-        "src/lib/**/*.ts",
+        // "src/lib/**/*.ts",
+        "src/types/**/*.ts",
         "types/**/*.d.ts",
       ],
       outDir: outputDir,
@@ -95,13 +96,25 @@ export default defineConfig({
     },
     outDir: outputDir,
     rollupOptions: {
-      external: ["#app", "vue", "zod", "immer", "lodash-es"],
+      external: ["#app", "vue", "zod", "immer", "lodash-es",
+        // /.*\.d\.ts$/, // regex to match any .d.ts file in the source
+      ],
       output: {
         // Allow Rollup to handle chunking automatically without manualChunks
         chunkFileNames: "chunks/[name]-[hash].js",
         // Optionally, set entryFileNames and assetFileNames if needed
         // entryFileNames: 'entry/[name].js',
         // assetFileNames: 'assets/[name].[hash][extname]',
+        // assetFileNames: (assetInfo) => {
+        //   // Exclude .d.ts files from being processed
+        //   const name = assetInfo.names[0]
+        //   if (!name) return ""
+
+        //   if (name.endsWith(".d.ts")) {
+        //     return "" // Don't include .d.ts files in the output
+        //   }
+        //   return "[name]" // For other files, use the default output name
+        // },
       },
       // Remove the commonjs plugin as Vite handles CommonJS internally
       // plugins: [commonjs()], // Removed

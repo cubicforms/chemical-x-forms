adfb8825b7a5fdadba3def4981759c40adf6f013
Oswald Chisala <ochisala@canarytechnologies.com>
2025-02-02 18:44:58 -0500

    demo of a non-working example we want to work on

--- playground/app.vue
+++ playground/app.vue
@@ -2,25 +2,23 @@
 // eslint-disable-next-line @stylistic/semi
 import { z } from "zod";
 
-const likeSchema = z.object({ likes: z.number() })
-const { getValue, setValue } = useForm({ schema: likeSchema })
-function increaseLikes() {
-  setValue("likes", c => ++c)
-}
-const likes = getValue()
+const schema = z.object({ count: z.number() })
+const { getValue, setValue } = useForm({ schema })
+const value = getValue()
 </script>
 
 <template>
   <div>
     <h1>Nuxt module playground!</h1>
     <hr>
-    <span>Number of likes:</span>
-    <hr>
-    <pre>{{ JSON.stringify(likes, null, 2) }}</pre>
-    <hr>
-    <button @click="increaseLikes">
-      Increase the number of likes
-    </button>
+    <h2>Value:</h2>
+    <pre>{{ JSON.stringify(value, null, 2) }}</pre>
+    <label for="count">Write the count</label>
+    <input
+      id="count"
+      type="text"
+      @input="(e) => setValue('count', (e.target as any)?.value)"
+    >
   </div>
 </template>
 

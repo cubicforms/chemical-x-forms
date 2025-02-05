12a7719e1008ce04a7be6e357bc01f59699db562
Oswald Chisala <ochisala@canarytechnologies.com>
2025-02-05 00:14:03 -0500

    wip playground showing what's pososible
    yes, there are 2 eslint errors. it's fine, this is a test branch

--- playground/app.vue
+++ playground/app.vue
@@ -2,34 +2,29 @@
 // eslint-disable-next-line @stylistic/semi
 import { z } from "zod";
 
-const schema = z.object({ count: z.number() })
-const { getValue, setValue, inputTracker } = useForm({ schema, key: "test" })
-const value = getValue()
+const schema = z.object({ address: z.object({ line1: z.string(), line2: z.string() }), count: z.number(), optional: z.record(z.string()) })
+const { getValue, setValue } = useForm({ schema: zodAdapter(schema), key: "test", initialState: { count: 4 } })
+const { currentValue, meta } = getValue("address", { withMeta: true })
 </script>
 
 <template>
   <div>
     <h1>Nuxt module playground!</h1>
     <hr>
-    <h2>Value:</h2>
-    <pre>{{ JSON.stringify(value, null, 2) }}</pre>
-    <label for="count">Write the count</label>
+    <pre>{{ currentValue }}</pre>
+    <pre>{{ meta }}</pre>
+    <label for="line1">Line1</label>
     <input
-      id="count"
       type="text"
-      @input="(e) => setValue('count', (e.target as any)?.value)"
+      placeholder="Enter line 1"
+      @input="(e) => setValue('address.line1', e.target.value)"
     >
-    <button @click="() => setValue({ peace: 'please', 9: [6] })">
-      Update at root (1)
-    </button>
-    <button @click="() => setValue({ hello: 'world' })">
-      Update at root (2)
-    </button>
-    <button @click="() => setValue({ peace: 'please!!!' })">
-      Update at root (1, update peace)
-    </button>
-    <hr>
-    <pre>{{ JSON.stringify(inputTracker, null, 2) }}</pre>
+    <label for="line2">Line2</label>
+    <input
+      type="text"
+      placeholder="Enter line 2"
+      @input="(e) => setValue('address.line2', e.target.value)"
+    >
   </div>
 </template>
 

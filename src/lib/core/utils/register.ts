976ea82ebf1c8d757b3d6c5161c10042ef8a248a
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-04 03:22:00 -0500

    wip: implementing register function (incomplete but working)

--- src/lib/core/utils/register.ts
+++ src/lib/core/utils/register.ts
@@ -1,25 +1,43 @@
-import type { AbstractSchema, FieldTransformer, FormKey } from "../../../types/types-api"
+import { get } from "lodash-es"
+import { toRef, type Ref } from "vue"
+import type { AbstractSchema, FieldTransformer, FormKey, FormStore, MetaTracker } from "../../../types/types-api"
 import type { GenericForm } from "../../../types/types-core"
+import { updateMetaTracker } from "../composables/use-meta-tracker-store"
+import type { XModelValue } from "../directives/xmodel"
+import { getForm } from "./get-value"
 
 // undefined by default (defer to useForm global setting)
 type RegisterContext<Input, Output> = {
   fieldTransformer?: undefined | boolean | FieldTransformer<Input, Output>
 }
 
-type AbstractHTMLEvent = { target?: { value: unknown } }
-
-type RegisterReturnType = {
-  "@input": (e: AbstractHTMLEvent) => void
-}
-
 export function registerFactory<Form extends GenericForm>(
+  formStore: Ref<FormStore<Form>>,
   formKey: FormKey,
   _schema: AbstractSchema<Form, Form>,
+  metaTracker: Ref<MetaTracker>,
+  setValue: (key: string, value: unknown) => boolean,
 ) {
-  function registerLogic<Input, Output>(path: string, context?: RegisterContext<Input, Output>) {
-    return { "@input": (e) => {
-      console.log(e.target?.value, { path, context, formKey })
-    } } satisfies RegisterReturnType
+  const form = getForm(formStore, formKey)
+  // TODO: use context
+  function registerLogic<Input, Output>(path: string, context?: RegisterContext<Input, Output>): XModelValue {
+    console.log({ context })
+    if (metaTracker.value?.[path] === undefined) {
+      updateMetaTracker({
+        basePath: path,
+        metaTracker: metaTracker.value,
+        rawValue: get(form, path),
+      })
+    }
+    return {
+      innerRef: toRef(() => metaTracker.value?.[path]?.rawValue),
+      registerElement: (el) => {
+        console.log("NOW WE CAN BUILD ELEMENT API\n\t>>\treceived an element from the xmodel directive!", el)
+      },
+      setValueWithInternalPath(value) {
+        return setValue(path, value)
+      },
+    }
   }
 
   return registerLogic

246455b268eedbf2ce82ca875ed9d8e293b81e8c
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-05 01:58:33 -0500

    implement an element state api

---
+++ src/lib/core/utils/element-state-api.ts
@@ -0,0 +1,27 @@
+import { get } from "lodash-es"
+import type { ComputedRef, Ref } from "vue"
+import { toRef } from "vue"
+import type { MetaTracker, MetaTrackerValue } from "../../../types/types-api"
+import type { GenericForm } from "../../../types/types-core"
+import type { ElementDOMState, ElementDOMStateStore } from "../composables/use-element-store"
+
+type ElementState<Value = unknown> = ElementDOMState & { currentValue: Value | undefined, meta: MetaTrackerValue }
+
+export function elementStateFactory<
+  Form extends GenericForm
+>(form: ComputedRef<Form>, metaTracker: Ref<MetaTracker>, elementDOMStateStoreRef: Ref<ElementDOMStateStore, ElementDOMStateStore>) {
+  function getElementState<Value = unknown>(path: string): Ref<ElementState<Value>> {
+    return toRef(() => {
+      const elementDomState = elementDOMStateStoreRef.value[path] ?? { focused: null, blurred: null }
+      const metaTrackerValue = metaTracker.value[path] ?? { rawValue: null, updatedAt: undefined }
+      const formValue = get(form.value, path) as Value | undefined
+      return {
+        ...elementDomState,
+        meta: metaTrackerValue,
+        currentValue: formValue,
+      }
+    })
+  }
+
+  return getElementState
+}

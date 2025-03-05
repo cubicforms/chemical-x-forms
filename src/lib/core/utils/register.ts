afa477f1b5069fb0c19d8dc18df88b47948ea33a
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-05 02:01:56 -0500

    implement function caching in the register factory function
    I was worried about potentially recomputing the same functions for large
    forms (avoiding memory leaks, listener de-registration bugs, etc), This
    is defensive code, I didn't actually notice any issues

--- src/lib/core/utils/register.ts
+++ src/lib/core/utils/register.ts
@@ -2,7 +2,7 @@ import { get } from "lodash-es"
 import { toRef, type Ref } from "vue"
 import type { AbstractSchema, FieldTransformer, FormKey, FormStore, MetaTracker } from "../../../types/types-api"
 import type { GenericForm } from "../../../types/types-core"
-import type { DeregisterElement, RegisterElement } from "../composables/use-element-store"
+import type { GetElementHelpers } from "../composables/use-element-store"
 import { updateMetaTracker } from "../composables/use-meta-tracker-store"
 import type { XModelValue } from "../directives/xmodel"
 import { getForm } from "./get-value"
@@ -17,11 +17,11 @@ export function registerFactory<Form extends GenericForm>(
   formKey: FormKey,
   _schema: AbstractSchema<Form, Form>,
   metaTracker: Ref<MetaTracker>,
-  _registerElement: RegisterElement,
-  _deregisterElement: DeregisterElement,
   setValue: (key: string, value: unknown) => boolean,
+  getElementHelpers: GetElementHelpers,
 ) {
   const form = getForm(formStore, formKey)
+  const elementHelperCache: Record<string, ReturnType<GetElementHelpers>> = {}
   // TODO: use context
   function registerLogic<Input, Output>(path: string, context?: RegisterContext<Input, Output>): XModelValue {
     console.log({ context })
@@ -36,11 +36,17 @@ export function registerFactory<Form extends GenericForm>(
       innerRef: toRef(() => metaTracker.value?.[path]?.rawValue),
       registerElement: (el) => {
         console.log("NOW WE CAN BUILD ELEMENT API\n\t>>\treceived an element from the xmodel directive!", el)
-        _registerElement(el, path)
+        if (!(path in elementHelperCache)) {
+          elementHelperCache[path] = getElementHelpers(path)
+        }
+        elementHelperCache[path].registerElement(el)
       },
       deregisterElement: (el) => {
         console.log("NOW WE CAN **DISCARD** ELEMENT FROM TRACKER\n\t>>\treceived an element from the xmodel directive!", el)
-        _deregisterElement(el, path)
+        if (!(path in elementHelperCache)) {
+          elementHelperCache[path] = getElementHelpers(path)
+        }
+        elementHelperCache[path].deregisterElement(el)
       },
       setValueWithInternalPath(value) {
         return setValue(path, value)
@@ -50,3 +56,12 @@ export function registerFactory<Form extends GenericForm>(
 
   return registerLogic
 }
+
+// type FullElementState<Value = unknown> = {
+//   currentValue: Value
+//   meta: MetaTrackerValue
+// } & ElementDOMStateStore
+
+// function elementStateApiFactory() {
+
+// }

7b0593c5d2c4cb38f33001cb9399ebced79de075
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-04 20:58:09 -0500

    wip: progress on the register utility
    I may move this function to wherever the setValueFactory is

--- src/lib/core/utils/register.ts
+++ src/lib/core/utils/register.ts
@@ -2,6 +2,7 @@ import { get } from "lodash-es"
 import { toRef, type Ref } from "vue"
 import type { AbstractSchema, FieldTransformer, FormKey, FormStore, MetaTracker } from "../../../types/types-api"
 import type { GenericForm } from "../../../types/types-core"
+import type { DeregisterElement, RegisterElement } from "../composables/use-element-store"
 import { updateMetaTracker } from "../composables/use-meta-tracker-store"
 import type { XModelValue } from "../directives/xmodel"
 import { getForm } from "./get-value"
@@ -16,6 +17,8 @@ export function registerFactory<Form extends GenericForm>(
   formKey: FormKey,
   _schema: AbstractSchema<Form, Form>,
   metaTracker: Ref<MetaTracker>,
+  _registerElement: RegisterElement,
+  _deregisterElement: DeregisterElement,
   setValue: (key: string, value: unknown) => boolean,
 ) {
   const form = getForm(formStore, formKey)
@@ -33,6 +36,11 @@ export function registerFactory<Form extends GenericForm>(
       innerRef: toRef(() => metaTracker.value?.[path]?.rawValue),
       registerElement: (el) => {
         console.log("NOW WE CAN BUILD ELEMENT API\n\t>>\treceived an element from the xmodel directive!", el)
+        _registerElement(el, path)
+      },
+      deregisterElement: (el) => {
+        console.log("NOW WE CAN **DISCARD** ELEMENT FROM TRACKER\n\t>>\treceived an element from the xmodel directive!", el)
+        _deregisterElement(el, path)
       },
       setValueWithInternalPath(value) {
         return setValue(path, value)

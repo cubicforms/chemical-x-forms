e46461223aab103008c43a0178dd5a576474391c
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-07 15:26:50 -0500

    wip: improve register logic
    The main change here is that we no longer attempt to guess the html
    element states (focused, blurred, touched, etc) on the server, but
    rather send a null placeholder. We also make sure that isConnected is
    always in sync with the appropriate html element states

--- src/lib/core/utils/register.ts
+++ src/lib/core/utils/register.ts
@@ -24,26 +24,37 @@ export function registerFactory<Form extends GenericForm>(
   const elementHelperCache: Record<string, ReturnType<GetElementHelpers>> = {}
   // TODO: use context
   function registerLogic<Input, Output>(path: string, _context?: RegisterContext<Input, Output>): XModelValue {
-    if (metaTracker.value?.[path] === undefined) {
-      updateMetaTracker({
-        basePath: path,
-        metaTracker: metaTracker.value,
-        rawValue: get(form, path),
-      })
-    }
     return {
       innerRef: toRef(() => metaTracker.value?.[path]?.rawValue),
       registerElement: (el) => {
-        if (!(path in elementHelperCache)) {
+        // use the metaTracker to invalidate the cache automatically on remount
+        if (!(path in elementHelperCache) || !metaTracker.value[path].isConnected) {
           elementHelperCache[path] = getElementHelpers(path)
         }
-        elementHelperCache[path].registerElement(el)
+        const success = elementHelperCache[path].registerElement(el)
+        if (success) {
+          updateMetaTracker({
+            basePath: path,
+            metaTracker: metaTracker.value,
+            rawValue: metaTracker.value?.[path]?.rawValue ?? get(form, path),
+            updateTime: false,
+            isConnected: true,
+          })
+        }
       },
       deregisterElement: (el) => {
         if (!(path in elementHelperCache)) {
           elementHelperCache[path] = getElementHelpers(path)
         }
-        elementHelperCache[path].deregisterElement(el)
+        const remainingElementCount = elementHelperCache[path].deregisterElement(el)
+        console.log("number of remaining elements", remainingElementCount)
+        updateMetaTracker({
+          basePath: path,
+          metaTracker: metaTracker.value,
+          rawValue: metaTracker.value?.[path]?.rawValue ?? get(form, path),
+          updateTime: false, // for consistency, only recompute `updatedAt` when form value changes
+          isConnected: !!remainingElementCount, // only mark as disconnected if all elements are unmounted
+        })
       },
       setValueWithInternalPath(value) {
         return setValue(path, value)

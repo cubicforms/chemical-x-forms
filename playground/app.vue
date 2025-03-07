277e8a850f3fbba2b816782df373ca8d7ee1b7ec
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-07 15:36:23 -0500

    app.vue showing what's possible right now

--- playground/app.vue
+++ playground/app.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 import { z } from "zod"
 
-const schema = z.object({ name: z.string().default("ozzy"), age: z.number() })
-const { register, getValue, setValue, getElementState } = useForm({ schema, key: "test-form" })
+const schema = z.object({ name: z.string().default("ozzy"), age: z.number().default(3) })
+const { register, getValue, setValue, getState } = useForm({ schema, key: "test-form", initialState: { name: "jack" } })
 const reg = register("name")
 const inn = reg.innerRef
 
 const mountTextArea = ref(true)
-const x = getElementState("name")
+const x = getState("name")
+const y = getState("age")
+
+const simple = ref(true)
 </script>
 
 <template>
@@ -15,8 +18,10 @@ const x = getElementState("name")
   <pre>{{ JSON.stringify(getValue().value, null, 2) }}</pre>
   field state:
   <pre>{{ JSON.stringify(inn, null, 2) }}</pre>
-  total element state:
+  total element state (name):
   <pre>{{ JSON.stringify(x, null, 2) }}</pre>
+  total element state (age):
+  <pre>{{ JSON.stringify(y, null, 2) }}</pre>
   <button @click="() => { setValue('name', 'ayra') }">
     update name to ayra
   </button>
@@ -30,6 +35,7 @@ const x = getElementState("name")
   >
   <hr>
   <input
+    v-if="simple"
     v-xmodel.number="register('name')"
   >
   <hr>
@@ -40,8 +46,11 @@ const x = getElementState("name")
   <textarea
     v-if="mountTextArea"
     v-xmodel="register('name')"
-    autofocus
   />
+  <hr>
+  <button @click="simple = !simple">
+    {{ simple ? 'remove' : 'bring back' }} simple text input
+  </button>
 </template>
 
 <style>

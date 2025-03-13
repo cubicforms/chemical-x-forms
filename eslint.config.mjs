037184269e3a13214bd156e60b4b7bc8b28b34fc
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-13 03:27:36 -0400

    chore: remove unused comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat"
 
-// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
   features: {
-    // Rules for module authors
     tooling: true,
     stylistic: {
       arrowParens: false,
@@ -29,23 +26,6 @@ export default createConfigForNuxt({
   .append(
     {
       rules: {
-        // "import/order": [
-        //   "error",
-        //   {
-        //     "groups": [
-        //       "builtin",
-        //       "external",
-        //       "internal",
-        //       ["sibling", "parent"],
-        //       "index",
-        //     ],
-        //     "newlines-between": "always",
-        //     "alphabetize": {
-        //       order: "asc",
-        //       caseInsensitive: true,
-        //     },
-        //   },
-        // ],
         "semi": "off",
         "import/order": "off",
       },

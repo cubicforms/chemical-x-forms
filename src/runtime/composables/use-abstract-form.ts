d3104b9f99a4c671cf0b676566a64e3a665d7b4f
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-15 02:49:52 -0400

    refactor: move UseAbstractFormReturnType type to types-api.ts

--- src/runtime/composables/use-abstract-form.ts
+++ src/runtime/composables/use-abstract-form.ts
@@ -1,29 +1,18 @@
-import type { Ref } from "vue"
-import type { FieldState } from "../lib/core/composables/use-field-store"
 import { useElementStore } from "../lib/core/composables/use-field-store"
 import { useFormKey } from "../lib/core/composables/use-form-key"
 import { useFormStore } from "../lib/core/composables/use-form-store"
 import { useMetaTrackerStore } from "../lib/core/composables/use-meta-tracker-store"
 import { fieldStateFactory } from "../lib/core/utils/field-state-api"
 import { getComputedSchema } from "../lib/core/utils/get-computed-schema"
-import type { RegisterContext } from "../lib/core/utils/register"
 import { registerFactory } from "../lib/core/utils/register"
-import type { SetValuePayload } from "../lib/core/utils/set-value"
 import type {
   AbstractSchema,
-  CurrentValueContext,
-  CurrentValueWithContext,
-  FormKey,
-  HandleSubmit,
-  UseFormConfiguration,
-  ValidationResponseWithoutValue,
-  XModelValue,
+  UseAbstractFormReturnType,
+  UseFormConfiguration
 } from "../types/types-api"
 import type {
   DeepPartial,
-  FlatPath,
-  GenericForm,
-  NestedType,
+  GenericForm
 } from "../types/types-core"
 
 export function useAbstractForm<
@@ -88,47 +77,4 @@ export function useAbstractForm<
     register,
     key,
   } satisfies UseAbstractFormReturnType<Form, GetValueFormType>
-}
-
-export type UseAbstractFormReturnType<
-  Form extends GenericForm,
-  GetValueFormType extends GenericForm = Form
-> = {
-  getFieldState: (path: FlatPath<Form, keyof Form, true>) => Ref<FieldState>
-  handleSubmit: HandleSubmit<Form>
-  getValue: {
-    (): Readonly<Ref<GetValueFormType>>
-    <Path extends FlatPath<Form>>(path: Path): Readonly<
-      Ref<NestedType<GetValueFormType, Path>>
-    >
-    <WithMeta extends boolean>(
-      context: CurrentValueContext<WithMeta>
-    ): WithMeta extends true
-      ? CurrentValueWithContext<GetValueFormType>
-      : Readonly<Ref<GetValueFormType>>
-    <Path extends FlatPath<Form>, WithMeta extends boolean>(
-      path: Path,
-      context: CurrentValueContext<WithMeta>
-    ): WithMeta extends true
-      ? CurrentValueWithContext<NestedType<GetValueFormType, Path>>
-      : Readonly<Ref<NestedType<GetValueFormType, Path>>>
-  }
-  setValue: {
-    <Value extends SetValuePayload<Form>>(value: Value): boolean
-    <
-      Path extends FlatPath<Form>,
-      Value extends SetValuePayload<NestedType<Form, Path>>
-    >(
-      path: Path,
-      value: Value
-    ): boolean
-  }
-  validate: (
-    path: string | undefined
-  ) => Readonly<Ref<ValidationResponseWithoutValue<Form>>>
-  register: (
-    path: FlatPath<Form, keyof Form, true>,
-    _context?: RegisterContext<typeof path, NestedType<Form, typeof path>>
-  ) => XModelValue<NestedType<Form, typeof path> | undefined>
-  key: FormKey
 }

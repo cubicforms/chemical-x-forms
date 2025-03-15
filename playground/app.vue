b85d93d60a56d58e4564cf7322b130044f08b4f2
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-15 06:30:33 -0400

    feat: app.vue showing us what's currently possible

--- playground/app.vue
+++ playground/app.vue
@@ -2,8 +2,15 @@
 import { z } from "zod"
 import { zodAdapter } from "../src/runtime/adapters/zod"
 
-const schema = z.object({ name: z.string().default("larry"), age: z.number(), address: z.object({ line1: z.string().default("123 main street") }) })
-const { register, getFieldState } = useAbstractForm({ schema: zodAdapter(schema), key: "user-form" })
+const schema = z.object({
+  name: z.string().default("larry"),
+  age: z.number(),
+  address: z.object({ line1: z.string().default("123 main street") }),
+})
+const { register, getFieldState } = useAbstractForm({
+  schema: zodAdapter(schema),
+  key: "user-form",
+})
 
 const state = getFieldState("address.line1")
 const mounted = ref(true)
@@ -16,7 +23,7 @@ const mounted = ref(true)
       v-xmodel="register('address.line1')"
     >
     <button @click="mounted = !mounted">
-      {{ mounted ? 'unmounted input' : 'mounted input' }}
+      {{ mounted ? "unmounted input" : "mounted input" }}
     </button>
     <pre>{{ JSON.stringify(state, null, 2) }}</pre>
   </div>

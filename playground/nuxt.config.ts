9122158e27254584d933fe6e8b64d296e0199e6c
Oswald Kay Chisala <79558128+ozzyfromspace@users.noreply.github.com>
2025-03-16 18:09:18 -0400

    Add full type safety for v-xmodel directive (#40)
    * wip: add ObjectDirective import statement to types/v-xmodel.d.ts
    
    * typo: revert path
    
    * feat: declare @chemical-x/forms/types as the types export path
    
    At the moment, we are exporting everything in
    src/runtime/types/types-api.ts . All other types are currently
    considered "private", but this will require a careful audit in the
    future. Because the library is not yet "stable", we allow this change
    
    * fix: update path for types/v-xmodel.ts in build output
    
    * refactor: move register directive types into types-api.ts and rename them
    
    The ultimate goal here is to expose RegisterDirective so that we can
    import it on the client via types/v-xmodel.d.ts and add it to the global
    vue directive types. This allows type inference to work.
    
    * chore: declare @chemical-x/forms/types alias in playground for type safety
    
    Note: this ONLY exposes the types from the src folder, nothing else.
    Thus, module logic is unaffected, only a type-inference improvement for
    the playground/app.vue template (v-xmodel generally screams when
    appropriate).
    
    Note that v-xmodel doesn't scream if NOTHING is passed to it (example,
    something like <input v-xmodel />) . This is strictly not correct, but I
    don't have the bandwidth to implement an eslint config that gets
    injected into the client app to handle this at the moment. Not the
    biggest concern, but just wanted to acknowledge the ts shortfall here.
    We may prioritize this bit of type-safety depending on practical usage.

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   devtools: { enabled: true },
+  alias: {
+    "@chemical-x/forms/types": "../src/runtime/types/type-api.ts"
+  },
   compatibilityDate: "2025-01-28",
 })

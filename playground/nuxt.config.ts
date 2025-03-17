084c0f1f0eca7a503c48fe4eb8861a7ace097d8e
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-16 20:39:18 -0400

    fix: typo in alias pathname

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   modules: ["../src/module"],
   devtools: { enabled: true },
   alias: {
-    "@chemical-x/forms/types": "../src/runtime/types/type-api.ts"
+    "@chemical-x/forms/types": "../src/runtime/types/types-api.ts"
   },
   compatibilityDate: "2025-01-28",
 })

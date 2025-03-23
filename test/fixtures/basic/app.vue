e98222b1eae43c6b65a5297661e9287ad7effbdf
Oswald Kay Chisala <79558128+ozzyfromspace@users.noreply.github.com>
2025-03-23 14:47:05 -0400

    create node matrix testing workflow (#76)
    * feat: add matrix testing file to identify issues in different node versions
    
    This ensures that we support all users. For example, Set.difference only
    works for node versions 22+
    
    Note: I have intentionally added v16 to the testing matrix, even though
    I don't plan on supporting it. I just want to see the test fail, so I
    can demonstrate the failure mode to myself
    
    * refactor(matrix.yml): specify pnpm version
    
    * refactor: switch to pnpm action setup @v2 (from github marketplace)
    
    * refactor: trying a new workflow file suggested by claude ai
    
    * fix: update pnpm version, remove --frozen-lockfile
    
    * fix: generate .nuxt folder so that test build works
    
    * chore(matrix.yml): simplify step title
    
    * chore(publish.yml): update checkout plugin to v4
    
    * test: introduce error on purpose (node v16 is unsupported)
    
    The pnpm toolchain won't work correctly, based on local testing. hoping
    to see an error.
    
    * chore: remove node v16 from support matrix, scope runs to main branch
    
    * test: introduce set.difference api to generate errors for matrix runs
    
    I will soon revert this to the safer utility function I wrote
    
    * feat(tsconfig.json): add test to included dirs, declare @runtime alias
    
    * feat(test): add runtime @runtime path alias definition for nuxt, add types
    
    * feat(test): use useform library in test app.vue to demo integration
    
    * feat: specify supported engines for node and pnpm
    
    At the time of writing this PR, we support node v18 up and pnpm 9.x up
    
    I haven't seen how far back we can take the pnpm. low priority testing
    for another time
    
    * refactor: switch back to safer setdifference utility
    
    Rolling back a test I did to see if the modern Set.difference API
    available in node versions 22.x and up would fail for v18 and v20 in the
    matrix tests, while v22 and LTS worked. Everything went as expected
    
    * chore(matrix.yml): explicitly add workflow job permissions
    
    Only readonly permissions are required.
    
    Responding to this warning from Github:
    
    Actions Job or Workflow does not set permissions
    
    with detail:
    
    If a GitHub Actions job or workflow has no explicit permissions set,
    then the repository permissions are used. Repositories created under
    organizations inherit the organization permissions. The organizations or
    repositories created before February 2023 have the default permissions
    set to read-write. Often these permissions do not adhere to the
    principle of least privilege and can be reduced to read-only, leaving
    the write permission only to a specific types as issues: write or
    pull-requests: write.
    
    Tool: CodeQL
    Rule ID: actions/missing-workflow-permissions
    Query source:
    https://github.com/github/codeql/blob/0ac0dad49df9d1a7312918ca897b277209dea046/actions/ql/src/Security/CWE-275/MissingActionsPermissions.ql

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -2,5 +2,11 @@
   <div>basic</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { z } from "zod"
+// import { useForm } from "../../../src/runtime/composables/use-form"
+import { useForm } from "@runtime/composables/use-form"
+
+const schema = z.object({ name: z.string() })
+useForm({ schema })
 </script>

b7a33c0330467682f4839fababb5222b6b5008d2
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-23 14:03:33 -0400

    feat(test): add runtime @runtime path alias definition for nuxt, add types

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,11 @@
+import { defineNuxtConfig, type DefineNuxtConfig } from "nuxt/config"
 import MyModule from "../../../src/module"
 
 export default defineNuxtConfig({
   modules: [
     MyModule,
   ],
-})
+  alias: {
+    "@runtime": "../../../src/runtime"
+  }
+}) as DefineNuxtConfig

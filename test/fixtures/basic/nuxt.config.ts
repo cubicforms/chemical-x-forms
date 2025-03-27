3a09c47128bd86f89444c30a62e70e8f8d43e9b1
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-27 01:23:35 -0400

    chore: remove default test fixture

--- test/fixtures/basic/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtConfig, type DefineNuxtConfig } from 'nuxt/config'
-import MyModule from '../../../src/module'
-
-export default defineNuxtConfig({
-  modules: [MyModule],
-  alias: {
-    '@runtime': '../../../src/runtime',
-  },
-}) as DefineNuxtConfig

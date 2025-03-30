69b6b83cec0dbf02c9a472c8f9a66f411c28b482
Oswald Chisala <ochisala@canarytechnologies.com>
2025-03-30 14:56:03 -0400

    fix(playground-tsc): ass explicit type checks so that playground app passes

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,3 +1,5 @@
+import { defineNuxtConfig, type DefineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   modules: [['../src/module', { useZod: true }]],
   devtools: { enabled: true },
@@ -5,4 +7,4 @@ export default defineNuxtConfig({
     '@chemical-x/forms/types': '../src/runtime/types/types-api.ts',
   },
   compatibilityDate: '2025-01-28',
-})
+}) as DefineNuxtConfig
